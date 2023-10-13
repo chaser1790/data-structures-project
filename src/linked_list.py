3b49cf57e635d3f8df3f60fb993e8c07f1e0ea47
chaser-omg <zolagalaxy1790@gmail.com>
2023-10-13 10:42:55 +0300

    Добавлены методы: `to_list()` и `get_data_by_id()`
    Добавлен в метод `get_data_by_id()` блок `try/except` для перехвата ошибки `TypeError` и печати в консоль сообщения о неподходящем формате данных.
    
    Добавлены тесты для методов `to_list()` и `get_data_by_id()`
    
    Код в файле `main.py` выдаёт ожидаемые значения

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,51 +1,86 @@
 class Node:
     def __init__(self, data, next_node=None):
-        self.data = data  # Значение узла
+        self.data = data  # Значение текущего узла
         self.next_node = next_node  # Ссылка на следующий узел
 
 
 class LinkedList:
+    """
+    Класс, представляющий односвязный список.
+    """
     def __init__(self):
-        self.head = None  # Начальный узел списка
-        self.tail = None  # Конечный узел списка
+        self.head = None  # Головной узел списка
+        self.tail = None  # Хвостовой узел списка
+
+    def __str__(self):
+        """
+        Метод для вывода связного списка в виде строки.
+
+        :return: строковое представление данных в списке
+        """
+        node = self.head
+        nodes = []
+        while node is not None:  # Пока узел не является пустым
+            nodes.append(str(node.data))  # Добавляем значение текущего узла в список
+            node = node.next_node  # Переходим к следующему узлу
+        nodes.append("None")
+        return " -> ".join(nodes)  # Преобразуем список в строку
 
     def insert_beginning(self, data: dict) -> None:
         """
-        Метод добавляет узел в начало списка.
+        Метод для добавления узла в начало связного списка.
 
         :param data: Значение для узла.
         """
         new_node = Node(data, self.head)  # Создаем новый узел
         if not self.head:  # Если список пуст
-            self.tail = new_node  # Новый узел становится концом списка
-        self.head = new_node  # Новый узел становится началом списка
+            self.tail = new_node  # Новый узел становится хвостовым узлом
+        self.head = new_node  # Новый узел становится головным узлом
 
     def insert_at_end(self, data: dict) -> None:
         """
-        Метод добавляет узел в конец списка.
+        Метод для добавления узла в конец связного списка.
 
         :param data: Значение для узла.
         """
         if not self.head:  # Если список пуст
-            self.insert_beginning(data)  # добавляем узел в начало
+            self.insert_beginning(data)  # Добавляем узел в начало
         else:
             new_node = Node(data)  # Создаем новый узел
-            self.tail.next_node = new_node  # Прошлой "хвост" ставим ссылку на новый узел
-            self.tail = new_node  # Назначаем новый хвост
+            self.tail.next_node = new_node  # Устанавливаем ссылку от предыдущего хвоста к новому узлу
+            self.tail = new_node  # Новый узел становится хвостовым
 
-    def __str__(self) -> str:
+    def to_list(self):
         """
-        Метод возвращает строковое представление списка.
+        Метод для преобразования связного списка в стандартный список Python.
+
+        :return: список со значениями узлов
         """
-        node = self.head  # Начинаем с головного узла
-        if node is None:  # Если список пуст
-            return str(None)
+        res_list = []  # Инициализируем пустой список
+        node = self.head
 
-        ll_string = str(node.data)  # Инициализируем строку с начальным узлом
-        node = node.next_node  # Двигаемся к следующему узлу
-        while node:  # Пока есть узлы в списке
-            ll_string += f' -> {str(node.data)}'  # Добавляем значение узла в строку
-            node = node.next_node  # Продолжаем двигаться по списку
+        while node:  # Пока узел не является пустым
+            res_list.append(node.data)  # Добавляем данные узла в список
+            node = node.next_node  # Переходим к следующему узлу
 
-        ll_string += ' -> None'  # Добавляем в конец '-> None', чтобы указать на конец списка
-        return ll_string
+        return res_list
+
+    def get_data_by_id(self, id):
+        """
+        Метод для поиска в списке данных с указанным id.
+
+        :param id: id, по которому производится поиск
+        :return: данные узла или None, если такого id нет в списке
+        """
+        node = self.head
+
+        while node:  # Пока узел не является пустым
+            try:
+                if node.data.get('id') == id:  # Если id узла соответствует заданному
+                    return node.data  # Возвращаем данные этого узла
+            except (TypeError, AttributeError):  # Если данные не являются словарем или в словаре нет элемента 'id'
+                print('Данные не являются словарем или в словаре нет ключа id.')
+
+            node = node.next_node  # Переходим к следующему узлу
+
+        return None  # Если в списке не найдено данных с заданным id
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -6,11 +6,9 @@ class TestLinkedList(unittest.TestCase):
         """
         Тестирование метода insert_beginning, который выполняет вставку элемента в начало связанного списка.
         """
-
         ll = LinkedList()  # Создаем пример связанного списка
         ll.insert_beginning({'id': 1})  # Вставляем элемент в начало списка
         self.assertEqual(str(ll), "{'id': 1} -> None")  # Проверяем корректность добавления элемента
-
         ll.insert_beginning({'id': 0})  # Добавляем еще один элемент в начало списка
         self.assertEqual(str(ll), "{'id': 0} -> {'id': 1} -> None")  # Проверяем корректность добавления элемента
 
@@ -18,25 +16,48 @@ class TestLinkedList(unittest.TestCase):
         """
         Тестирование метода insert_at_end, который выполняет вставку элемента в конец связанного списка.
         """
-
         ll = LinkedList()  # Создаем пример связанного списка
         ll.insert_at_end({'id': 1})  # Вставляем элемент в конец списка
         self.assertEqual(str(ll), "{'id': 1} -> None")  # Проверяем корректность добавления элемента
-
         ll.insert_at_end({'id': 2})  # Добавляем еще один элемент в конец списка
         self.assertEqual(str(ll), "{'id': 1} -> {'id': 2} -> None")  # Проверяем корректность добавления элемента
+
+    def test_to_list(self):
+        """
+        Тестирование метода to_list.
+        """
+        ll = LinkedList()
+        ll.insert_beginning({'id': 0, 'username': 'serebro'})
+        ll.insert_at_end({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        lst = ll.to_list()
+        self.assertEqual(lst, [{'id': 0, 'username': 'serebro'}, {'id': 1, 'username': 'lazzy508509'}, {'id': 3, 'username': 'mosh_s'}])
+
+    def test_get_data_by_id(self):
+        """
+        Тестирование метода get_data_by_id.
+        """
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        ll.insert_beginning({'id': 0, 'username': 'serebro'})
+
+        data = ll.get_data_by_id(3)
+        self.assertEqual(data, {'id': 3, 'username': 'mosh_s'})
+
+        data = ll.get_data_by_id(100)  # id, which does not exist
+        self.assertEqual(data, None)
 
     def test_combo_insert(self):
         """
         Тестирование комбинированного использования методов insert_beginning и insert_at_end.
         """
-
         ll = LinkedList()  # Создаем пример связанного списка
         ll.insert_beginning({'id': 1})  # Вставляем элемент в начало списка
         ll.insert_at_end({'id': 2})  # Вставляем элемент в конец списка
         ll.insert_at_end({'id': 3})  # Вставляем еще один элемент в конец списка
         ll.insert_beginning({'id': 0})  # Вставляем элемент в начало списка
-
         # Проверяем корректность комбинации вставки элементов в начало и конец списка
         self.assertEqual(str(ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
 
